2cc145319525f42cafc0e45b3c3e8950a42ec6b9
Simraan Khatib <99394288+SimraanK@users.noreply.github.com>
2022-12-06 19:15:22 +0530

    Add files via upload

---
+++ fullweb2.js
@@ -0,0 +1,13 @@
+
+
+function dropdownMenu() {
+    let x = document.getElementById("dropdownClick");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        // change topnav to topnav.responsive
+        
+    } else {
+        x.className = "topnav"; 
+        
+    }
+}
